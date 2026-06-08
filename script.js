c0a7ecf208509dfa5e7453fd90cdf0bb4d6176e9
Sourabh Singh <67355597+Sourabhsingh021@users.noreply.github.com>
2026-06-08 11:29:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+const parent = document.getElementById("parent");
+draggedItem = null;
+
+parent.addEventListener('dragstart', (e) => {
+draggedItem = e.target;
+})
+
+parent.addEventListener("dragover", (e) => {
+e.preventDefault();
+});
+
+parent.addEventListener("drop", (e) => {
+e.preventDefault();
+
+const target = e.target;
+
+if (
+	target.classList.contains("image") &&
+	target !== draggedItem
+) {
+	const draggedNext = draggedItem.nextElementSibling;
+	const targetNext = target.nextElementSibling;
+
+	parent.insertBefore(draggedItem, targetNext);
+	parent.insertBefore(target, draggedNext);
+}
+});
